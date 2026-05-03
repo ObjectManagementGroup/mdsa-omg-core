755b3671c1dd18c4bdddbc0858ec132ff88fc4a6
Jason McC. Smith <jason@elementalreasoning.com>
2026-05-03 15:58:19 -0700

    Containerization support

--- _core.mk
+++ _core.mk
@@ -1,19 +1,26 @@
 .PHONY: gen clean core local md images rfp spec debug
 
+# Several things break if only plain sh is used
+SHELL := /bin/bash
+
 # Target 'debug' re-enables output from LaTeX run
 _outputstream := 2>&1 > /dev/null
 debug: _outputstream := ""
 debug: all
 
-rfp: ${build} rfp_core local md images
-	@echo --- Creating PDF
-	mv ${build}/RFP.tex ${build}/${pdfnamebase}.tex
-	cd build && latexmk -bibtex -pdf -auxdir=. -outdir=.. ./${pdfnamebase}.tex ${_outputstream}
+# Set if not set in Makefile for local LaTeX installation
+build ?= ./build
+source ?= /source
 
-spec: ${build} ${gencondir} core local md images
+rfp: "${build}" rfp_core local md images
 	@echo --- Creating PDF
-	mv ${build}/Specification.tex ${build}/${pdfnamebase}.tex
-	cd build && latexmk -bibtex -pdf -auxdir=. -outdir=.. ./${pdfnamebase}.tex ${_outputstream}
+	mv "${build}/RFP.tex" "${build}/${pdfnamebase}.tex"
+	cd "${build}" && latexmk -bibtex -pdf -auxdir=. -outdir="${source}" "./${pdfnamebase}.tex" ${_outputstream}
+
+spec: "${build}" ${gencondir} core local md images
+	@echo --- Creating PDF
+	mv "${build}/Specification.tex" "${build}/${pdfnamebase}.tex"
+	cd "${build}" && latexmk -bibtex -pdf -auxdir=. -outdir="${source}" "./${pdfnamebase}.tex" ${_outputstream}
 
 # Only generate from the model if there is an appropriate ${specacro}.config file. I.e. UML.config or BPMN.config.
 gen: ${gencondir}
@@ -29,7 +36,7 @@ ${gencondir}:
 
 clean:
 	@echo --- Cleaning
-	rm -rf build/
+	rm -rf "${build}"
 	rm -f "${pdfnamebase}.pdf"
 
 ${build}:
@@ -37,19 +44,19 @@ ${build}:
 
 # This is a raw copy, it could be smarter, but this is sufficiently fast
 ${build}/GeneratedContent: ${build}
-	cp -R ./GeneratedContent "${build}/GeneratedContent"
+	cp -R "${source}/GeneratedContent" "${build}/GeneratedContent"
 
 
 # LaTeX files support (local and core)
 coretex := $(wildcard mdsa-omg-core/*.sty) $(wildcard mdsa-omg-core/*.bib) $(wildcard mdsa-omg-core/*.yaml) 
-localtex := $(wildcard ./*.tex) $(wildcard ./*.bib)
-markdowns := $(filter-out ./README.md, $(wildcard ./*.md))
+localtex := $(wildcard ${source}/*.tex) $(wildcard ${source}/*.bib)
+markdowns := $(filter-out ${source}/README.md, $(wildcard ${source}/*.md))
 core: $(subst mdsa-omg-core,${build},${coretex} $(wildcard mdsa-omg-core/*.tex) )
 rfp_core: $(subst mdsa-omg-core,${build},$(wildcard mdsa-omg-core/RFP*) ${coretex})
 spec_core: $(subst mdsa-omg-core,${build},$(wildcard mdsa-omg-core/*pecification*) ${coretex})
-local: $(subst ./,${build}/,${localtex}) 
-md: ${build} $(subst ./,${build}/,$(subst .md,.tex,${markdowns}))
-imagefiles := $(subst ./,${build}/,$(wildcard ./Images/*.svg))
+local: $(subst ${source}/,${build}/,${localtex}) 
+md: ${build} $(subst ${source}/,${build}/,$(subst .md,.tex,${markdowns}))
+imagefiles := $(subst ${source}/,${build}/,$(wildcard ${source}/Images/*.svg))
 images: ${imagefiles}
 # echo ${imagefiles}
 # ${build}/Images: ${build}
@@ -61,15 +68,15 @@ images: ${imagefiles}
 ${build}/%.tex: mdsa-omg-core/%.tex
 	cp $< $@
 
-${build}/%.tex: ./%.tex
+${build}/%.tex: ${source}/%.tex
 	cp $< $@
 
 # Zero length files converted by pandoc end up non-zero-length, which breaks OMG templates
 # Don't convert empty files, just touch
-${build}/%.tex: ./%.md
+${build}/%.tex: ${source}/%.md
 	@size=$(shell du -ks $< | cut -f1) ; \
 	if (( $${size} > 0 )); then \
-		pandoc $< -f markdown --defaults ./${build}/omgLaTeX.yaml -t latex -o $@ ; \
+		pandoc $< -f markdown --defaults "${build}/omgLaTeX.yaml" -t latex -o $@ ; \
 	else \
 		touch $@ ; \
 	fi
@@ -81,16 +88,16 @@ ${build}/%.sty: mdsa-omg-core/%.sty
 ${build}/%.bib: mdsa-omg-core/%.bib
 	cp $< $@
 
-${build}/%.bib: ./%.bib
+${build}/%.bib: ${source}/%.bib
 	cp $< $@
 
 ${build}/%.yaml: mdsa-omg-core/%.yaml
 	cp $< $@
 
-${build}/%.yaml: ./%.yaml
+${build}/%.yaml: ${source}/%.yaml
 	cp $< $@
 
 # SVG is the only format formally accepted by OMG for documents
-${build}/Images/%.svg: ./Images/%.svg
+${build}/Images/%.svg: ${source}/Images/%.svg
 	echo $<
 	cp $< $0
